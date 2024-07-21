9261b06d42a93662758879894755d0a3384d74d6
Aman Chopra <amanchopra.atg@gmail.com>
2024-07-21 18:02:18 +0530

    revamping the blog page

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Navbar from './Navbar'
 
-// import BlogSection from './BlogSection'; // Import BlogSection component
-
 const Blog = () => {
   return (
     <div className='container'>
       <Navbar />
-      <h1>Blog Page</h1>
+      <p><b>Beware!! </b><i>The Room of Requirement</i> still holds the traces of <i>He-Who-Must-Not-Be-Named's </i> dark magic.</p>
     </div>
   );
 };
