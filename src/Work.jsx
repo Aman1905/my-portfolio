e0e4e685567a6dcb8b9d9decc205739f7edcac32
Aman Chopra <amanchopra.atg@gmail.com>
2024-10-26 22:04:52 +0530

    enhanced the UI for work section

--- src/Work.jsx
+++ src/Work.jsx
@@ -23,23 +23,34 @@ const Work = () => {
                 <TimelineConnector />
               </TimelineSeparator>
               <TimelineContent>
-                <div className="blog-card">
-                  <div className="blog-card-description">
-                    <h3>Developer Relations - Technical Writer @ LambdaTest</h3><br/>
-                    <p>
-                      <ul>
-                        <li>Collaborate closely with product managers, developers, and QA engineers to create user-friendly documentation ensuring that complex technical concepts were communicated effectively to end-users.</li><br/>
-                        <li>Conducted regular content audits and updates to keep documentation aligned with software updates and customer feedback, improving user experience and reducing support inquiries.</li><br />
-                        <li>Authored and maintained a range of documentation, including user guides, API documentation, release notes, and knowledge base articles, consistently receiving positive feedback for clarity and accuracy.</li><br />
-                        <li>Responsible for creating code samples, tutorials, and blog posts to assist developers in integrating our platform into their applications, fostering developer engagement.</li><br />
-                      </ul>
-                    </p>
-                    <h6>Continuous Integration (CI) · Cross-team Collaboration · GitHub · Continuous Delivery (CD) · Technical Documentation · YAML · Docusaurus · Testing · Communica · Technical Writing · Git · Blogging</h6>
+                <div className="work-outer-card">
+                  <div className="work-heading">
+                    <h3>Developer Relations - Technical Writer@<a href='https://lambdatest.com/' target='_blank' rel="noreferrer">LambdaTest</a></h3>
+                  </div>
+                  <div className="work-description-row">
+                    <div className="work-description">
+                      <p><strong>Cross-functional Collaboration:</strong> Worked closely with product managers, developers, and QA engineers to develop clear, user-friendly <a href='https://www.lambdatest.com/support/docs/' target='_blank' rel="noreferrer">documentation</a> that effectively communicated complex technical concepts to end-users.</p>
+                    </div>
+                    <div className="work-description">
+                      <p><strong>Content Audits and Continuous Improvement:</strong> Conducted regular audits and updates to ensure documentation stayed current with software updates and customer insights, enhancing user experience and reducing support inquiries.</p>
+                    </div>
+                  </div>
+                  <div className="work-description-row">
+                    <div className="work-description">
+                      <p><strong>Comprehensive Documentation:</strong> Authored and maintained diverse documentation assets—including user guides, <a href='https://www.lambdatest.com/support/api-doc/' target='_blank' rel="noreferrer">API documentation</a>, <a href='https://www.lambdatest.com/support/docs/hyperexecute-release-notes/' target='_blank' rel="noreferrer">release notes</a>, and <a href='https://www.lambdatest.com/blog/author/amanchopraatg/' target='_blank' rel="noreferrer">knowledge base articles</a>—consistently praised for clarity and precision.</p>
+                    </div>
+                    <div className="work-description">
+                      <p><strong>Developer Engagement:</strong> Created <a href='https://github.com/john190501?tab=repositories' target='_blank' rel="noreferrer">code samples</a>, <a href='https://youtube.com/playlist?list=PLZMWkkQEwOPkTh2Q6WYpExWoQ8yzqjZ1C&si=J3V2NanZ-eif26YN' target='_blank' rel="noreferrer">tutorials</a>, and <a href='https://www.lambdatest.com/blog/author/amanchopraatg/' target='_blank' rel="noreferrer">blog posts</a> to guide developers in integrating our platform, significantly boosting developer engagement and support.</p>
+                    </div>
+                  </div>
+                  <div className="work-heading">
+                    <h6>GitHub Actions, Swagger/OpenAPI, Postman, Technical Documentation Writing (Markdown), YAML</h6>
                   </div>
                 </div>
               </TimelineContent>
             </TimelineItem>
             {/* lambdatest end */}
+
             {/* udyamwell start */}
             <TimelineItem>
               <TimelineOppositeContent color="text.secondary">June 2023 - Present</TimelineOppositeContent>
@@ -48,23 +59,34 @@ const Work = () => {
                 <TimelineConnector />
               </TimelineSeparator>
               <TimelineContent>
-                <div className="blog-card">
-                  <div className="blog-card-description">
-                    <h3>Technical Project Manager @ UdyamWell</h3><br/>
-                    <p>
-                      <ul>
-                        <li>Led a cross-functional website development team (frontend, backend, content) to deliver a high-performance website.</li><br/>
-                        <li>Oversaw cloud infrastructure and DevOps practices on the Azure platform, ensuring efficient website deployment and ongoing maintenance.</li><br />
-                        <li>Spearheaded website design and user experience (UX) to create a user-friendly and engaging online presence.</li><br />
-                        <li>Successfully deployed the website on the Azure platform, leveraging automation tools and best practices.</li><br />
-                      </ul>
-                    </p>
-                    <h6>DevOps · User Experience (UX) · Azure cloud · Website Design · Content Management · Technical Project Management</h6>
+                <div className="work-outer-card">
+                  <div className="work-heading">
+                    <h3>Technical Project Manager@<a href='https://www.udyamwell.com/' target='_blank' rel="noreferrer">UdyamWell</a></h3>
+                  </div>
+                  <div className="work-description-row">
+                    <div className="work-description">
+                      <p><strong>Cross-functional Team Leadership:</strong> Led a frontend, backend, and content team to deliver a high-performance website with seamless collaboration.</p>
+                    </div>
+                    <div className="work-description">
+                      <p><strong>Azure Cloud Infrastructure & DevOps:</strong> Oversaw cloud infrastructure and implemented DevOps practices on Azure for efficient deployment and ongoing maintenance.</p>
+                    </div>
+                  </div>
+                  <div className="work-description-row">
+                    <div className="work-description">
+                      <p><strong>User-Centric Design & UX:</strong> Spearheaded website design and user experience to ensure a user-friendly, engaging online presence.</p>
+                    </div>
+                    <div className="work-description">
+                      <p><strong>Efficient Deployment:</strong> Successfully deployed the website on Azure, leveraging automation tools and best practices to optimize performance and scalability.</p>
+                    </div>
+                  </div>
+                  <div className="work-heading">
+                    <h6>Azure App Services, and Azure Storage, Azure Container Registry, GitHub Actions, CI/CD, Figma, React.js, Node.js,  MongoDB</h6>
                   </div>
                 </div>
               </TimelineContent>
             </TimelineItem>
             {/* udyamwell end */}
+
             {/* geeksforgeeks start */}
             <TimelineItem>
               <TimelineOppositeContent color="text.secondary">Oct 2022 - Jun 2023</TimelineOppositeContent>
@@ -73,27 +95,34 @@ const Work = () => {
                 <TimelineConnector />
               </TimelineSeparator>
               <TimelineContent>
-                <div className="blog-card">
-                  <div className="blog-card-description">
-                    <h3>Project Coordinator @ GeeksforGeeks</h3><br/>
-                    <p>
-                      <ul>
-                        <li>Responsible for the Planning and Scheduling of the projects, Communicating with stakeholders, Managing and Administring the Project team, and Resolving issues.</li><br/>
-                        <li>Responsible for hiring the new talents in the various teams - DevOps, Gate CS, Python, GBlog, Freelancers.</li><br />
-                        <li>Introduced various new practices to increase the DevOps Content growth.</li><br />
-                        <li>Worked on the Technical Blogs and other content related to DevOps in the GeeksforGeeks.</li><br />
-                        <li>Reviewed and improved the articles on DevOps and Cloud tools.</li><br/>
-                        <li>Increased the traffic view of the DevOps content by ~22%.</li><br />
-                        <li>Written some in-house articles as well on various Technologies.</li><br />
-                        <li>Responsible for the maintenance of quality content on DevOps and Cloud domain.</li><br />
-                      </ul>
-                    </p>
-                    <h6>Azure DevOps · Kubernetes · DevOps · GitHub · Product Management · Cloud Computing · Google Cloud Platform (GCP) · Git · Blogger · Azure · Amazon Web Services (AWS) · Docker · Business Growth · Leadership</h6>
+                <div className="work-outer-card">
+                  <div className="work-heading">
+                    <h3>Project Coordinator@GeeksforGeeks</h3>
+                  </div>
+                  <div className="work-description-row">
+                    <div className="work-description">
+                      <p>Achieved a <strong>22% increase in DevOps content traffic </strong>through strategic enhancements in technical blogs, video content, and project-based articles.</p>
+                    </div>
+                    <div className="work-description">
+                      <p><strong>Project Planning, Team Management, and Stakeholder Communication: </strong>Led project planning, scheduling, and issue resolution while effectively managing a cross-functional project team. Maintained consistent communication with stakeholders to ensure alignment and progress.</p>
+                    </div>
+                  </div>
+                  <div className="work-description-row">
+                    <div className="work-description">
+                      <p><strong>Talent Acquisition and Content Development Initiatives:</strong> Spearheaded hiring efforts across DevOps, Gate CS, Python, GBlog, and freelance teams, bringing in high-quality talent. Introduced new practices to enhance DevOps content growth and maintain quality standards.</p>
+                    </div>
+                    <div className="work-description">
+                      <p><strong>Content Review, Quality Control, and Knowledge Sharing:</strong>Reviewed and refined articles on DevOps and Cloud tools, ensuring accuracy and quality. Authored in-house articles on various technologies, contributing to the team's knowledge base and content library.</p>
+                    </div>
+                  </div>
+                  <div className="work-heading">
+                    <h6>Git and GitHub, AWS, Azure, GCP, Docker, Kubernetest, Google Analytics, SEMrush, Product Management</h6>
                   </div>
                 </div>
               </TimelineContent>
             </TimelineItem>
             {/* geeksforgeeks end */}
+            
             {/* niit start */}
             <TimelineItem>
               <TimelineOppositeContent color="text.secondary">Nov 2021 - Sep 2023</TimelineOppositeContent>
@@ -102,19 +131,28 @@ const Work = () => {
                 <TimelineConnector />
               </TimelineSeparator>
               <TimelineContent>
-                <div className="blog-card">
-                  <div className="blog-card-description">
-                    <h3>Technical Mentor @ National Institute for Industrial Training</h3><br/>
-                    <p>
-                      <ul>
-                        <li>Project Course - Full Stack Web Development: Created project-based course using HTML5, CSS3, JavaScript, PHP, MySQL, and Azure.</li><br/>
-                        <li>Tutorial - Introduction to Full Stack Web Development and DS Algo with Java: Created tutorial (Recorded Live Video and Documentation) for HTML5, CSS3, JS, PHP, MySQL, Azure, and Java.</li><br />
-                        <li>Responsible for leading Web Team and building various projects for the organization.</li><br />
-                        <li>Impact: The course has been taken by 150+ students so far with 4.65 average ratings.</li><br />
-                        <li>Product Management along with the Mentoring of the mentees assigned. Agile Methodology was followed for building the products.</li><br/>
-                      </ul>
-                    </p>
-                    <h6>Mentoring · Programming · Node.js · GitHub · PHP · HTML5 · MySQL · MongoDB · JavaScript · Team Leadership · Git · Project Management · Core Java · Amazon Web Services (AWS) · React.js</h6>
+                <div className="work-outer-card">
+                  <div className="work-heading">
+                    <h3>Technical Mentor@National Institute for Industrial Training</h3>
+                  </div>
+                  <div className="work-description-row">
+                    <div className="work-description">
+                      <p><strong>Project Course - Full Stack Web Development:</strong> Developed a hands-on, project-driven course covering HTML5, CSS3, JavaScript, PHP, MySQL, and Azure, designed to build foundational full-stack skills.</p>
+                    </div>
+                    <div className="work-description">
+                      <p><strong>Tutorial - Full Stack Web Development and Data Structures & Algorithms with Java:</strong> Created a comprehensive tutorial series, including recorded live sessions and documentation, featuring HTML5, CSS3, JavaScript, PHP, MySQL, Azure, and Java.</p>
+                    </div>
+                  </div>
+                  <div className="work-description-row">
+                    <div className="work-description">
+                      <p><strong>Leadership and Project Development:</strong> Led the Web Team in designing and delivering multiple projects for the organization, driving impactful outcomes.</p>
+                    </div>
+                    <div className="work-description">
+                      <p><strong>Impact:</strong> Successfully trained 150+ students with a strong 4.65 average course rating, reflecting high engagement and satisfaction.</p>
+                    </div>
+                  </div>
+                  <div className="work-heading">
+                    <h6>HTML5, CSS3, JavaScript, PHP, MySQL, Git and GitHub, Azure</h6>
                   </div>
                 </div>
               </TimelineContent>
